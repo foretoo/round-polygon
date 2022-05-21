8b771203ef3b862322a5ab9d8d906d9cc4eaff82
Sergey Borovikov <serg.there@gmail.com>
2022-05-21 13:37:04 +0300

    renames

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,18 @@
 import { Point } from "./types"
 
-export const find_length = (
+export const getLength = (
   A: Point, B: Point,
 ) => Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2))
 
-export const find_angle = (
+export const getAngle = (
   A: Point, B: Point, C?: Point,
 ) => {
   if (!C)
     return Math.atan2(B.y - A.y, B.x - A.x)
   else {
-    const AB = find_length(A, B),
-          BC = find_length(B, C),
-          CA = find_length(C, A)
+    const AB = getLength(A, B),
+          BC = getLength(B, C),
+          CA = getLength(C, A)
 
     return Math.acos((AB*AB + BC*BC - CA*CA) / (2*AB*BC))
   }
@@ -20,7 +20,7 @@ export const find_angle = (
 
 const PI = Math.PI, TAU = PI * 2
 
-export const get_clock_dir = (
+export const getClockDir = (
   angle1: number, angle2: number
 ) => {
   const diff = angle2 - angle1
@@ -31,25 +31,24 @@ export const get_clock_dir = (
   )
 }
 
-export const get_angles = (
+export const getAngles = (
   prev_point: Point,
   curr_point: Point,
   next_point: Point,
 ) => {
   const
-    main = find_angle(prev_point, curr_point, next_point),
-    prev = find_angle(curr_point, prev_point),
-    next = find_angle(curr_point, next_point),
-    dir  = get_clock_dir(prev, next),
+    main = getAngle(prev_point, curr_point, next_point),
+    prev = getAngle(curr_point, prev_point),
+    next = getAngle(curr_point, next_point),
+    vel = 1 / Math.tan(main / 2),
+    dir  = getClockDir(prev, next),
     bis  = prev + dir * main / 2
   
-  return { main, next, prev, bis, dir }
+  return { main, next, prev, vel, dir, bis }
 }
 
-export const getprev =
-  <T>(i: number, arr: T[]): T =>
-    arr[(i - 1 + arr.length) % arr.length]
+export const getPrev = <T>(i: number, arr: T[]): T =>
+  arr[(i - 1 + arr.length) % arr.length]
 
-export const getnext =
-  <T>(i: number, arr: T[]): T =>
-    arr[(i + 1) % arr.length]
+export const getNext = <T>(i: number, arr: T[]): T =>
+  arr[(i + 1) % arr.length]
