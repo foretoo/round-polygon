64b9f8e7278b0c41dbcd76dd1c8e3b4eeb48e5a9
Sergey Borovikov <serg.there@gmail.com>
2022-06-12 00:05:34 +0300

    demo v0.6.4

--- public/demo.js
+++ public/demo.js
@@ -1,98 +1,60 @@
-import { getcanvas, circle, shape, vertex, CLOSE, clear, fill, stroke, arc, text, font, settext, frame, loop, animate } from "https://unpkg.com/bratik@latest/lib/bratik.es.js"
-import roundPolygon from "../lib/round-polygon.es.js"
-
-const
-  { ctx, height, width } = getcanvas(),
-  pointnum = 6,
-  grey = "#0007",
-  skin = "#f407",
-  highlight = "#e02",
-  padding = -Math.min(width, height) / 10,
-  radiusrange = document.querySelector("input"),
-  radiusvalue = document.querySelector("#radiusvalue")
-
-ctx.lineCap = "round"
-ctx.lineJoin = "round"
-
-let points = [], polygon
-for (let i = 0; i < pointnum; i++) points[i] = { x: width / 2, y: height / 2 }
-polygon = roundPolygon(points, +radiusrange.value)
-draw()
-
-const animatepoint = (p) => {
-  const
-    moveX = animate(8000, "cubicInOut"),
-    moveY = animate(8000, "cubicInOut"),
-    newpoint = getrandpoint()
-
-  moveX(p, { x: newpoint.x })
-  moveY(p, { y: newpoint.y })
-}
-points.forEach(animatepoint)
-
+import { getcanvas, circle, shape, vertex, CLOSE, fill, stroke, frame, loop, stop, looping, rect, animate, text, font, settext } from "./bratik.js"
+import { Polio } from "./Polio.js"
+const { ctx, height, width } = getcanvas(), radius = Number.MAX_SAFE_INTEGER, mg = Math.min(width, height) / 3, fun = Object.assign({ image: "🏄" }, randtxt(mg / 2))
+const verpoly = new Polio(9, radius, width + mg * 2, height + mg * 2)
+verpoly.initgradient()
+verpoly.update(8000)
+const horpoly = new Polio(13, radius, width + mg, height + mg)
+horpoly.initgradient()
+ctx.shadowOffsetY = 20
+font(64)
+settext("center", "middle")
 const play = () => {
-  if (frame % 480 === 0) points.forEach(animatepoint)
-  polygon = roundPolygon(points, +radiusrange.value)
-  draw()
+  loop(() => {
+    if (frame % 480 === 0) verpoly.update(8000)
+    if ((frame - 320) % 480 === 0) horpoly.update(8000)
+    if ((frame - 160) % 480 === 0) animate(8000)(fun, Object.assign({}, randtxt(mg / 2)))
+    bg("black")
+    verpoly.draw()
+    ctx.drawImage(verpoly.image, -mg * 2, -mg * 2)
+    lines(verpoly.points, mg)
+    centres(verpoly.rounded, "white", mg)
+    horpoly.draw()
+    ctx.drawImage(horpoly.image, -mg, -mg)
+    lines(horpoly.points, mg / 2)
+    centres(horpoly.rounded, "black", mg / 2)
+    ctx.shadowColor = "#0004"
+    ctx.shadowBlur = 20
+    text(fun.image, fun.x, fun.y)
+    ctx.shadowColor = "transparent"
+    ctx.shadowBlur = 0
+  })
 }
-loop(play)
-
-radiusvalue.textContent = radiusrange.value
-radiusrange.oninput = (e) => {
-  const target = e.target
-  radiusvalue.textContent = target.value
-  polygon = roundPolygon(points, +radiusrange.value)
-  draw()
+function bg(color) {
+  fill(color)
+  rect(0, 0, width, height)
 }
-
-function draw() {
-  clear()
-  stroke(grey, 0.5)
+function lines(points, mg) {
+  stroke("#fff4", 0.5)
   fill(null)
   shape()
-  points.forEach((p) => vertex(p.x, p.y))
+  points.forEach((p) => vertex(p.x - mg, p.y - mg))
   shape(CLOSE)
-  stroke(null)
-  fill(grey)
-  points.forEach((p) => circle(p.x, p.y, 1))
-  if (points.length > 2) {
-    fill(skin)
+}
+function centres(polygon, color, mg) {
+  polygon.forEach((p) => {
     stroke(null)
-    shape()
-    polygon.forEach((p, i) => {
-      if (!i) vertex(p.in.x, p.in.y)
-      p.angle.main > 3e-4 && arc(p.x, p.y, p.next.x, p.next.y, p.arc.radius)
-      vertex(p.next.in.x, p.next.in.y)
-    })
-    shape(CLOSE)
-    polygon.forEach((p, i) => {
-      //// Centers of roundings
-      stroke(null)
-      fill(highlight)
-      circle(p.arc.x, p.arc.y, 3)
-      //// Arcs of roundings, stroked
-      if (p.angle.main > 3e-4) {
-        fill(null)
-        stroke(highlight, 4)
-        shape()
-        vertex(p.in.x, p.in.y)
-        arc(p.x, p.y, p.out.x, p.out.y, p.arc.radius)
-        shape()
-      }
-      //// Points numbers
-      stroke(null)
-      fill(grey)
-      font(14)
-      settext("center", "middle")
-      const { bis } = p.angle, x = p.x - Math.cos(bis) * 24, y = p.y - Math.sin(bis) * 24
-      text(`${i}`, x, y)
-    })
-  }
+    fill(color)
+    circle(p.arc.x - mg, p.arc.y - mg, 3)
+  })
 }
-
-function getrandpoint() {
+function randtxt(mg) {
   return {
-    x: padding + Math.random() * (width - padding * 2),
-    y: padding + Math.random() * (height - padding * 2),
+    x: mg + Math.random() * (width - mg * 2),
+    y: mg + Math.random() * (height - mg * 2),
   }
 }
+document.onkeyup = ({ code }) => {
+  code === "Space" && (looping ? stop() : play())
+}
+play()
