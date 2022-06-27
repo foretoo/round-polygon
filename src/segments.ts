cb2e47a42e716eded84fee45a66bc7fd94306b98
Sergey Borovikov <serg.there@gmail.com>
2022-06-27 11:26:34 +0300

    fix: set min amount to 1, return initial { x, y } if zero-opt

--- src/segments.ts
+++ src/segments.ts
@@ -7,22 +7,25 @@ const getSegments = (
   opt: number
 ) => {
 
+  if (!opt) return points.map((p) => ({ x: p.x, y: p.y }))
+
   const formedPolygon = points.reduce((
     acc: Point[], point
   ) => {
+
     const
       { angle: { prev, next }, arc: { x, y, radius }} = point,
       dir = point.angle.dir * -1,
       startangle = prev + dir * PI / 2,
       angle = (dir * PI + next - prev) % PI,
-      amount = type === "LENGTH" ? Math.round(dir * angle * radius / opt) : opt,
+      amount = type === "LENGTH" ? Math.round(dir * angle * radius / opt) || 1 : opt,
       unitangle = angle / amount,
       segments = []
 
     for (let i = 0; i <= amount; i++) {
       segments.push({
         x: x + Math.cos(startangle + unitangle * i) * radius,
-        y: y + Math.sin(startangle + unitangle * i) * radius
+        y: y + Math.sin(startangle + unitangle * i) * radius,
       })
     }
 
