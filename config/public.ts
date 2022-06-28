9257e2c25f05ce85647ae790c73741f6f4f53973
Sergey Borovikov <serg.there@gmail.com>
2022-06-28 18:59:28 +0300

    redefined

--- config/public.ts
+++ config/public.ts
@@ -4,21 +4,26 @@ import commonjs from "@rollup/plugin-commonjs"
 import esbuild from "rollup-plugin-esbuild"
 import { eslintBundle } from "rollup-plugin-eslint-bundle"
 import sass from "rollup-plugin-sass"
+import { terser } from "rollup-plugin-terser"
 
-export default defineConfig([
-  {
-    input: "./src/integration/index.ts",
-    output: {
-      file: "./public/index.js",
-      format: "es",
-      exports: "named",
-    },
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      esbuild(),
-      eslintBundle({ eslintOptions: { fix: true }}),
-      sass({ output: "./public/style.css" }),
-    ],
-  }
-])
+export default defineConfig({
+  external: (id) => id.includes("src/index.ts") || null,
+  input: "src/integration/index.ts",
+  output: {
+    file: "public/script.js",
+    format: "es",
+    paths: (id) =>
+        id.includes("src/index.ts") ? "../dist/round-polygon.es.js"
+      : id === "bratik"             ? "https://unpkg.com/bratik@latest/lib/bratik.es.js"
+      : id === "simplex-noise"      ? "https://unpkg.com/simplex-noise@3.0.1/dist/esm/simplex-noise.js"
+      : "",
+  },
+  plugins: [
+    nodeResolve({ jail: "src" }),
+    commonjs(),
+    esbuild(),
+    eslintBundle({ eslintOptions: { fix: true }}),
+    sass({ output: "public/style.css" }),
+    terser(),
+  ],
+})
